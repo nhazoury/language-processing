79640cc37a5597d27771396b73d3e40b006cae17
nhazoury <n.hazoury@live.com>
2021-11-06 00:56:01 +0000

    Test cases, tidying splitUp. Begin process method

--- src/StringProcessor.java
+++ src/StringProcessor.java
@@ -9,20 +9,38 @@ public class StringProcessor {
     /* Splits string into words without spaces as a List of strings.
     Existing functions already do this, but why not try from scratch?
      */
-    public List<String> splitUp(String str) {
+    public List<String> splitUp(String str, char divider, List<Character> important) {
         ArrayList<String> ans = new ArrayList<>();
         StringBuilder word = new StringBuilder();
+
+        // just to make sure the final word is actually included
         str = str + ' ';
+
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (c != ' ') {
+            if (c == divider) {
+                word = split(word, ans);
+            } else if (important.contains(c)) {
+                word = split(word, ans);
                 word.append(c);
             } else {
-                ans.add(word.toString());
-                word = new StringBuilder();
+                word.append(c);
             }
         }
 
         return ans;
+    }
+
+    private StringBuilder split(StringBuilder currentWord, List<String> sentence) {
+        sentence.add(currentWord.toString());
+        return new StringBuilder();
+    }
+
+    /* Only pass maths into this!! */
+    public Expression process(List<String> expressions) {
+        for (String str : expressions) {
+            // TODO: process maths, stack?, etc.
+        }
+        return null;
     }
 }
---
+++ tests/StringProcessorTests.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert.*;
+
+public class StringProcessorTests {
+    private StringProcessor strProc = new StringProcessor();
+
+    @Test
+    public void tokenisesWordsInEnglishSentence() {
+        String str = "Not sure what to put here";
+        List<String> result = strProc.splitUp(str, ' ', new ArrayList<>(List.of('(', ')')));
+        List<String> answer = new ArrayList<>(List.of("Not", "sure", "what", "to", "put", "here"));
+
+        Assert.assertEquals(answer, result);
+    }
+
+    @Test
+    public void tokenisesNumbers() {
+        String str = "1 + (2 - 3)";
+        List<String> result = strProc.splitUp(str, ' ', new ArrayList<>(List.of('(', ')')));
+        List<String> answer = new ArrayList<>(List.of("1", "+", "(", "2", "-", "3", ")"));
+
+        Assert.assertEquals(answer, result);
+    }
+
+
+}
